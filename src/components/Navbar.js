a65e551453f756ac12e6418f4e140d841b756c27
santiagomillan <santiagomillan@usantotomas.edu.co>
2022-04-06 18:55:03 -0500

    Primer commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components'
+
+const Nav = styled.nav`
+    height:60px;
+    background:red;
+`
+
+const Navbar = () => {
+  return (
+    <>
+        <Nav>
+            holi
+        </Nav>
+    </>
+  )
+}
+
+export default Navbar
